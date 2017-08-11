fff4058093e6111bd7c2c72b633b0c0fcc360351
toavina <toavinar@outlook.com>
2017-08-11 15:26:43 +0300

    avec my change

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,24 +1,24 @@
 require 'exponent-server-sdk'
 
 class TokensController < ApplicationController
-
   def create
-    message = ''
-
     # You probably actually want to associate this with a user,
     # otherwise it's not particularly useful
     @token = Token.where(value: params[:token][:value]).first
 
-    if @token.blank?
+    message = ''
+    if @token.present?
+      message = 'Welcome back!'
+    else
       @token = Token.create(token_params)
-      message = 'Hello there! This is a push notification.'
-
-      exponent.publish(
-        exponentPushToken: @token.value,
-        message: message,
-        data: {a: 'b'}, # Any arbitrary data to include with the notification
-      )
+      message = 'Welcome to Expo'
     end
+
+    exponent.publish(
+      exponentPushToken: @token.value,
+      message: message,
+      data: {a: 'b'}, # Data is required, pass any arbitrary data to include with the notification
+    )
 
     render json: {success: true}
   end
@@ -32,5 +32,4 @@ class TokensController < ApplicationController
   def exponent
     @exponent ||= Exponent::Push::Client.new
   end
-
 end
